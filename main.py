b26a71fc0d018fc76f6817678c2bc040160a87e9
Salick <61610355+saalick@users.noreply.github.com>
2021-11-04 21:19:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
 
-    bot_token = '2042256304:AAExE_LpS1nsBRXYZp-Rbz2YsaDmZMGQDFk'
+    bot_token = '2042256304:AAEkOR4bRxiWb35D4PAQut9b_ifXVQDEBRo'
     bot_chatID = '@junglebookcrypto'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
