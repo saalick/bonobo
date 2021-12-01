832cd20caed0237098cdf489d2902887d72921f9
Salick <61610355+saalick@users.noreply.github.com>
2021-12-01 22:41:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def telegram_bot_sendtext(bot_message):
     response = requests.get(send_text)
     return response.json()
 
-msg = ''' 💎For presale visit us at🌐 
-http://bit.ly/3cDVHGm
-💰The only method of payment the jungle accepts is BNB and Buy Through PinkSale with the Pinksale link http://bit.ly/3cDVHGm 
-Min buy 0.1 BNB max buy 5 BNB.Ask our Jungle Guides for more info.⚜️ '''
+msg = '''  💎For LIVE JBC Presale visit 🌐 http://bit.ly/3cDVHGm
+Min buy 0.1 BNB max buy 5 BNB.
+
+Check pinned video for "How to buy JBC on Pinksale" for instructions.
+
+Ask our Jungle Guides for more info.⚜️ '''
 while(True):
  telegram_bot_sendtext(msg)
  time.sleep(3600)
